6e75fa90681c1fa7f8f4fe7e43574e83a9de69c0
sidzmani <siddharthmmani@gmail.com>
2024-01-14 12:27:52 -0800

    Created responsive layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Display from "./left_sidebar/LeftSidebar";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <div>
-      <Display />
+      <Grid templateAreas={{ base: `"main"`, lg: `"aside main"` }}>
+        <Show above="lg">
+          <GridItem area="aside" bg="red">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="blue">
+          Main
+        </GridItem>
+      </Grid>
     </div>
   );
 }
