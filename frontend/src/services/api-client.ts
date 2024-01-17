1737f35ccb18d2f29dca1a959c45d0b59c170099
sidzmani <siddharthmmani@gmail.com>
2024-01-17 12:11:54 -0800

    Fixed base URL

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios, { CanceledError } from "axios";
 
 export default axios.create({
-  baseURL: "http://104.199.117.235:8000/api",
+  baseURL: "https://104.199.117.235:8000/api",
 });
 
 export { CanceledError };
